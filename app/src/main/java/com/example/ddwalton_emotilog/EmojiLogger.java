f35cf0cd46b408dd05285683db587ca1fd0e7f45
Dion Walton <ddwalton@ualberta.ca>
2025-09-30 00:23:30 -0600

    added some backend stuff

---
+++ app/src/main/java/com/example/ddwalton_emotilog/EmojiLogger.java
@@ -0,0 +1,9 @@
+package com.example.ddwalton_emotilog;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class EmojiLogger {
+    HashMap<String, Date> emojiLog;
+}
